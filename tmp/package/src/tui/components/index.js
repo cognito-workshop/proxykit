cd62154a15136b9fcfcb9297db1a9ecbff8a9e99
sectersion <oliverbigbee@gmail.com>
2026-08-04 18:17:02 +0100

    fix: remove shebang from src/cli.js, add tmp/ to gitignore

--- tmp/package/src/tui/components/index.js
+++
@@ -1,9 +0,0 @@
-export { Header } from './Header.js';
-export { StepSelector } from './StepSelector.js';
-export { TextInput } from './TextInput.js';
-export { ProgressBar } from './ProgressBar.js';
-export { FileTree } from './FileTree.js';
-export { ThemePreview } from './ThemePreview.js';
-export { ConfirmScreen } from './ConfirmScreen.js';
-export { GeneratingScreen } from './GeneratingScreen.js';
-export { DoneScreen } from './DoneScreen.js';
--- tmp/package/src/tui/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'ink';
-import { App } from './App.js';
-
-export function runTui(options = {}) {
-  const { waitUntilExit } = render(React.createElement(App, { options }));
-  return waitUntilExit();
-}
